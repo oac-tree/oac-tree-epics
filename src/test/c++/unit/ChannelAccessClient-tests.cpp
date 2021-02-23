e6dfd80408b9b903f6fc2167a2f0dc922b24ea54
bbauvir <bertrand.bauvir@iter.org>
2021-02-23 16:58:23 +0000

    Provide simple Gtest to verify CA notification mechanism

---
+++ src/test/c++/unit/ChannelAccessClient-tests.cpp
@@ -0,0 +1,149 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP Sequencer
+ *
+ * Description   : Unit test code
+ *
+ * Author        : B.Bauvir (IO)
+ *
+ * Copyright (c) : 2010-2020 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+// Global header files
+
+#include <gtest/gtest.h> // Google test framework
+
+#include <common/BasicTypes.h>
+#include <common/TimeTools.h>
+
+#include <common/AnyValueHelper.h>
+
+#include <common/ChannelAccessClient.h>
+
+// Local header files
+
+// Constants
+
+#undef LOG_ALTERN_SRC
+#define LOG_ALTERN_SRC "unit-test"
+
+// Type declaration
+
+// Function declaration
+
+static inline bool ExecuteSystemCall (const ccs::types::char8 * const command);
+
+// Global variables
+
+static ccs::log::Func_t _log_handler = ccs::log::SetStdout();
+
+// Function definition
+
+static inline bool ExecuteSystemCall (const ccs::types::char8 * const command)
+{
+
+  bool status = (0 == std::system(command));
+
+  if (status)
+    {
+      log_info("ExecuteSystemCall - Invoking command '%s' successful", command);
+    }
+  else
+    {
+      log_error("ExecuteSystemCall - Error with '%s' command", command);
+    }
+
+  return status;
+
+}
+
+static inline bool Initialise (void)
+{
+
+  bool status = ccs::HelperTools::Exist("../resources/ChannelAccessClient.db");
+
+  if (status)
+    {
+      status = ExecuteSystemCall("/usr/bin/screen -d -m /usr/bin/softIoc -d ../resources/ChannelAccessClient.db &> /dev/null");
+    }
+  else
+    {
+      status = ExecuteSystemCall("/usr/bin/screen -d -m /usr/bin/softIoc -d ./src/test/resources/ChannelAccessClient.db &> /dev/null");
+    }
+
+  if (status)
+    {
+      (void)ccs::HelperTools::SleepFor(1000000000ul);
+      status = ExecuteSystemCall("/usr/bin/caget SEQ-TEST:BOOL SEQ-TEST:FLOAT > /tmp/softioc-initialise.log");
+    }
+
+  if (status)
+    {
+      status = (NULL_PTR_CAST(ccs::base::ChannelAccessClient*) != ccs::base::ChannelAccessInterface::GetInstance<ccs::base::ChannelAccessClient>());
+    }
+
+  return status;
+
+}
+
+static inline bool Terminate (void)
+{
+
+  ccs::base::ChannelAccessInterface::Terminate<ccs::base::ChannelAccessClient>();
+
+  bool status = ExecuteSystemCall("/usr/bin/kill -9 `/usr/sbin/pidof softIoc` &> /dev/null");
+
+  return status;
+
+}
+
+// Function definition
+
+TEST(ChannelAccessClient, Launch)
+{
+
+  bool status = Initialise();
+
+  ccs::base::SharedReference<ccs::types::AnyType> _type; // Placeholder
+
+  if (status)
+    {
+      status = (0u < ccs::HelperTools::Parse(_type, "{\"type\":\"string\"}"));
+    }
+
+  if (status)
+    {
+      status = ccs::base::ChannelAccessInterface::GetInstance<ccs::base::ChannelAccessClient>()->AddVariable("SEQ-TEST:BOOL", ccs::types::AnyputVariable, _type);
+    }
+
+  if (status)
+    {
+      (void)ccs::base::ChannelAccessInterface::GetInstance<ccs::base::ChannelAccessClient>()->Launch();
+      (void)ccs::HelperTools::SleepFor(1000000000ul);
+    }
+
+  if (status)
+    { // Verify CA notification callbacks do  work
+      status = (false == ccs::HelperTools::IsUndefinedString(reinterpret_cast<char*>(ccs::base::ChannelAccessInterface::GetInstance<ccs::base::ChannelAccessClient>()->GetVariable("SEQ-TEST:BOOL")->GetInstance())));
+    }
+
+  if (status)
+    {
+      status = Terminate();
+    }
+
+  ASSERT_EQ(true, status);
+
+}
+
+#undef LOG_ALTERN_SRC
