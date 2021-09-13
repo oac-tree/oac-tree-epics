aa6119bb2ec993fe2a98a9d1324332a7aa0ff680
Van Herck Walter <Walter.Vanherck@iter.org>
2021-09-13 13:16:36 +0000

    Add unit test for extended CA Variable

--- src/test/c++/unit/ChannelAccessVariable-tests.cpp
+++ src/test/c++/unit/ChannelAccessVariable-tests.cpp
@@ -46,201 +46,125 @@
 #undef LOG_ALTERN_SRC
 #define LOG_ALTERN_SRC "unit-test"
 
-// Type declaration
+static const std::string BOOLCONNECTEDTYPE =
+  R"RAW({"type":"boolconnected","attributes":[{"value":{"type":"bool"}},{"connected":{"type":"bool"}}]})RAW";
 
-// Function declaration
-
-// Global variables
 
 static ccs::log::Func_t _log_handler = ccs::log::SetStdout();
 
-// Function definition
-
-static inline bool Initialise (void)
+class ChannelAccessVariableTest : public ::testing::Test
 {
+protected:
+  ChannelAccessVariableTest();
+  virtual ~ChannelAccessVariableTest();
 
-  bool status = ::ccs::HelperTools::Exist("../resources/ChannelAccessClient.db");
+  bool init_success;
+};
 
-  if (status)
-    {
-      status = ::ccs::HelperTools::ExecuteSystemCall(
-        "/usr/bin/screen -d -m -S cavariabletestIOC /usr/bin/softIoc -d ../resources/ChannelAccessClient.db &> /dev/null");
-    }
-  else
-    {
-      status = ::ccs::HelperTools::ExecuteSystemCall(
-        "/usr/bin/screen -d -m -S cavariabletestIOC /usr/bin/softIoc -d ./target/test/resources/ChannelAccessClient.db &> /dev/null");
-    }
+// Tests
 
-  return status;
-
-}
-
-static inline bool Terminate (void)
+TEST_F(ChannelAccessVariableTest, GetValue_success)
 {
-
-  bool status = ::ccs::HelperTools::ExecuteSystemCall("/usr/bin/screen -S cavariabletestIOC -X quit &> /dev/null");
-
-  return status;
-
-}
-
-// Function definition
-
-TEST(ChannelAccessVariable, GetValue_success)
-{
+  ASSERT_TRUE(init_success);
 
   auto variable = sup::sequencer::GlobalVariableRegistry().Create("ChannelAccessVariable");
+  ASSERT_TRUE(static_cast<bool>(variable));
 
-  bool status = static_cast<bool>(variable);
-
-  if (status)
-    {
-      status = Initialise();
-    }
-
-  if (status)
-    { // Setup implicit with AddAttribute .. access as 'string'
-      status = (variable->AddAttribute("channel", "SEQ-TEST:BOOL") &&
-                variable->AddAttribute("datatype", "{\"type\":\"string\"}"));
-    }
+  // Setup implicit with AddAttribute .. access as 'string'
+  ASSERT_TRUE(variable->AddAttribute("channel", "SEQ-TEST:BOOL") &&
+              variable->AddAttribute("datatype", "{\"type\":\"string\"}"));
 
   ccs::types::AnyValue value; // Placeholder
 
-  if (status)
-    {
-      status = variable->GetValue(value);
-    }
+  ASSERT_TRUE(variable->GetValue(value));
+  EXPECT_TRUE(ccs::types::String == value.GetType());
 
-  if (status)
-    {
-      status = (ccs::types::String == value.GetType());
-    }
-
-  if (status)
-    {
-      ccs::types::char8 buffer [1024];
-      ccs::HelperTools::SerialiseToJSONStream(&value, buffer, 1024u);
-      log_info("TEST(ChannelAccessVariable, GetValue_success) - Value is ..");
-      log_info("'%s'", buffer);
-    }
-
-  (void)Terminate();
-
-  ASSERT_EQ(true, status);
-
+  ccs::types::char8 buffer [1024];
+  ccs::HelperTools::SerialiseToJSONStream(&value, buffer, 1024u);
+  log_info("TEST(ChannelAccessVariable, GetValue_success) - Value is ..");
+  log_info("'%s'", buffer);
 }
 
-TEST(ChannelAccessVariable, GetValue_error)
+TEST_F(ChannelAccessVariableTest, GetValue_extended)
 {
+  ASSERT_TRUE(init_success);
 
   auto variable = sup::sequencer::GlobalVariableRegistry().Create("ChannelAccessVariable");
+  ASSERT_TRUE(static_cast<bool>(variable));
 
-  bool status = static_cast<bool>(variable);
-
-  if (status)
-    { // Missing mandatory attribute .. Setup implicit with AddAttribute
-      status = variable->AddAttribute("irrelevant", "undefined");
-    }
+  ASSERT_TRUE(variable->AddAttribute("channel", "SEQ-TEST:BOOL") &&
+              variable->AddAttribute("datatype", BOOLCONNECTEDTYPE));
 
   ccs::types::AnyValue value; // Placeholder
 
-  if (status)
-    {
-      status = ((false == variable->GetValue(value)) &&
-                (false == static_cast<bool>(value.GetType())));
-    }
+  ASSERT_TRUE(variable->GetValue(value, "value"));
+  EXPECT_TRUE(ccs::types::Boolean == value.GetType());
 
-  ASSERT_EQ(true, status);
+  ccs::types::AnyValue connected; // Placeholder
 
+  ASSERT_TRUE(variable->GetValue(connected, "connected"));
+  EXPECT_TRUE(ccs::types::Boolean == connected.GetType());
 }
 
-TEST(ChannelAccessVariable, SetValue_success)
+TEST_F(ChannelAccessVariableTest, GetValue_error)
 {
+  ASSERT_TRUE(init_success);
 
   auto variable = sup::sequencer::GlobalVariableRegistry().Create("ChannelAccessVariable");
+  ASSERT_TRUE(static_cast<bool>(variable));
 
-  bool status = static_cast<bool>(variable);
+  // Missing mandatory attribute .. Setup implicit with AddAttribute
+  EXPECT_TRUE(variable->AddAttribute("irrelevant", "undefined"));
 
-  if (status)
-    {
-      status = Initialise();
-    }
+  ccs::types::AnyValue value; // Placeholder
 
-  if (status)
-    { // Setup implicit with AddAttribute .. access as 'float32'
-      status = (variable->AddAttribute("channel", "SEQ-TEST:FLOAT") &&
-                variable->AddAttribute("datatype", "{\"type\":\"float32\"}"));
-    }
-
-  ccs::types::AnyValue value (static_cast<ccs::types::float32>(0.1));
-
-  if (status)
-    {
-      status = variable->SetValue(value);
-    }
-
-  if (status)
-    {
-      (void)ccs::HelperTools::SleepFor(100000000ul);
-    }
-
-  if (status)
-    {
-      status = variable->GetValue(value);
-    }
-
-  if (status)
-    {
-      ccs::types::char8 buffer [1024];
-      ccs::HelperTools::SerialiseToJSONStream(&value, buffer, 1024u);
-      log_info("TEST(ChannelAccessVariable, SetValue_success) - Value is ..");
-      log_info("'%s'", buffer);
-    }
-
-  if (status)
-    {
-      value = static_cast<ccs::types::float32>(7.5); // Should be truncated by the IOC record
-      status = variable->SetValue(value);
-    }
-
-  if (status)
-    {
-      (void)ccs::HelperTools::SleepFor(100000000ul);
-    }
-
-  if (status)
-    {
-      status = variable->GetValue(value);
-    }
-
-  if (status)
-    {
-      ccs::types::char8 buffer [1024];
-      ccs::HelperTools::SerialiseToJSONStream(&value, buffer, 1024u);
-      log_info("TEST(ChannelAccessVariable, SetValue_success) - Value is ..");
-      log_info("'%s'", buffer);
-    }
-
-  (void)Terminate();
-
-  ASSERT_EQ(true, status);
-
+  EXPECT_FALSE(variable->GetValue(value));
+  EXPECT_FALSE(static_cast<bool>(value.GetType()));
 }
 
-TEST(ChannelAccessVariable, ProcedureFile)
+TEST_F(ChannelAccessVariableTest, SetValue_success)
 {
+  ASSERT_TRUE(init_success);
 
+  auto variable = sup::sequencer::GlobalVariableRegistry().Create("ChannelAccessVariable");
+  ASSERT_TRUE(static_cast<bool>(variable));
+
+  // Setup implicit with AddAttribute .. access as 'float32'
+  EXPECT_TRUE(variable->AddAttribute("channel", "SEQ-TEST:FLOAT"));
+  EXPECT_TRUE(variable->AddAttribute("datatype", "{\"type\":\"float32\"}"));
+
+  ccs::types::AnyValue value(static_cast<ccs::types::float32>(0.1));
+
+  EXPECT_TRUE(variable->SetValue(value));
+  (void)ccs::HelperTools::SleepFor(100000000ul);
+
+  EXPECT_TRUE(variable->GetValue(value));
+  ccs::types::float32 val = value;
+  EXPECT_FLOAT_EQ(val, 0.1f);
+
+  value = static_cast<ccs::types::float32>(7.5);
+  EXPECT_TRUE(variable->SetValue(value));
+
+  (void)ccs::HelperTools::SleepFor(100000000ul);
+
+  EXPECT_TRUE(variable->GetValue(value));
+  val = value;
+  EXPECT_FLOAT_EQ(val, 7.5f);
+}
+
+TEST_F(ChannelAccessVariableTest, ProcedureFile)
+{
+  ASSERT_TRUE(init_success);
   std::string file; // Placeholder
 
   if (::ccs::HelperTools::Exist("../resources/variable_ca.xml"))
-    {
-      file = std::string("../resources/variable_ca.xml");
-    }
+  {
+    file = std::string("../resources/variable_ca.xml");
+  }
   else
-    {
-      file = std::string("./target/test/resources/variable_ca.xml");
-    }
+  {
+    file = std::string("./target/test/resources/variable_ca.xml");
+  }
 
   sup::sequencer::gtest::NullUserInterface ui;
   auto proc = sup::sequencer::ParseProcedureFile(file);
@@ -253,10 +177,32 @@ TEST(ChannelAccessVariable, ProcedureFile)
     (void)ccs::HelperTools::SleepFor(100000000ul); // Let system breathe
     proc->ExecuteSingle(&ui);
     exec = proc->GetStatus();
-  }
-  while ((sup::sequencer::ExecutionStatus::SUCCESS != exec) &&
-         (sup::sequencer::ExecutionStatus::FAILURE != exec));
+  } while ((sup::sequencer::ExecutionStatus::SUCCESS != exec) &&
+           (sup::sequencer::ExecutionStatus::FAILURE != exec));
   EXPECT_EQ(exec, sup::sequencer::ExecutionStatus::SUCCESS);
+}
+
+ChannelAccessVariableTest::ChannelAccessVariableTest()
+  : init_success{false}
+{
+  if (::ccs::HelperTools::Exist("../resources/ChannelAccessClient.db"))
+  {
+    init_success = ::ccs::HelperTools::ExecuteSystemCall(
+        "/usr/bin/screen -d -m -S cavariabletestIOC /usr/bin/softIoc -d ../resources/ChannelAccessClient.db &> /dev/null");
+  }
+  else
+  {
+    init_success = ::ccs::HelperTools::ExecuteSystemCall(
+        "/usr/bin/screen -d -m -S cavariabletestIOC /usr/bin/softIoc -d ./target/test/resources/ChannelAccessClient.db &> /dev/null");
+  }
+}
+
+ChannelAccessVariableTest::~ChannelAccessVariableTest()
+{
+  if (init_success)
+  {
+    ::ccs::HelperTools::ExecuteSystemCall("/usr/bin/screen -S cavariabletestIOC -X quit &> /dev/null");
+  }
 }
 
 #undef LOG_ALTERN_SRC
