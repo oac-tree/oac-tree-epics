b5fc942a4149547dd797a9d04f64746d84c48814
Van Herck Walter <Walter.Vanherck@iter.org>
2021-09-14 12:43:18 +0000

    Add unit test for PV write instruction and remove RegisterType instruction

--- src/test/c++/unit/PVAccessWriteInstruction-tests.cpp
+++ src/test/c++/unit/PVAccessWriteInstruction-tests.cpp
@@ -6,9 +6,9 @@
  *
  * Description   : Unit test code
  *
- * Author        : B.Bauvir (IO)
+ * Author        : Walter Van Herck (IO)
  *
- * Copyright (c) : 2010-2020 ITER Organization,
+ * Copyright (c) : 2010-2021 ITER Organization,
  *                 CS 90 046
  *                 13067 St. Paul-lez-Durance Cedex
  *                 France
@@ -25,13 +25,19 @@
 
 #include <common/BasicTypes.h>
 #include <common/TimeTools.h>
+
 #include <common/AnyValueHelper.h>
 
+#include <common/ChannelAccessClient.h>
+
 #include <SequenceParser.h>
+
 #include <Variable.h>
 #include <VariableRegistry.h>
 
 // Local header files
+
+#include "SystemCall.h"
 
 #include "NullUserInterface.h"
 
@@ -40,41 +46,53 @@
 #undef LOG_ALTERN_SRC
 #define LOG_ALTERN_SRC "unit-test"
 
-static ccs::log::Func_t _log_handler = ccs::log::SetStdout();
-
 static const std::string PVACCESSWRITEPROCEDURE = R"RAW(<?xml version="1.0" encoding="UTF-8"?>
 <Procedure xmlns="http://codac.iter.org/sup/sequencer" version="1.0"
            name="Trivial procedure for testing purposes"
            xmlns:xs="http://www.w3.org/2001/XMLSchema-instance"
            xs:schemaLocation="http://codac.iter.org/sup/sequencer sequencer.xsd">
-    <Repeat maxCount="10">
-        <Sequence>
-            <Wait name="wait"
-                timeout="1.0"/>
-            <Copy input="curr-time" output="pvxs-variable.timestamp"/>
-            <Copy input="curr-time" output="pvxs-other.timestamp"/>
-            <LogTrace name="variable"
-                input="pvxs-variable"/>
-            <LogTrace name="other"
-                input="pvxs-other"/>
-        </Sequence>
-    </Repeat>
+    <RegisterType jsontype='{"type":"seq::pva_write_test::Type/v1.0","attributes":[{"value":{"type":"float32"}}]}'/>
+    <Sequence>
+        <Copy input="old-pvxs-value" output="pvxs-variable"/>
+        <Equals lhs="pvxs-variable" rhs="old-pvxs-value"/>
+        <Wait timeout="2.0"/>
+        <PVAccessWriteInstruction name="write to pv"
+            channel="seq::test::variable"
+            variable="new-pvxs-value"/>
+        <Wait timeout="2.0"/>
+        <Equals lhs="pvxs-variable" rhs="new-pvxs-value"/>
+    </Sequence>
     <Workspace>
-        <SystemClock name="curr-time"/>
         <PVServerVariable name="pvxs-variable"
             channel="seq::test::variable"
-            datatype='{"type":"seq::test::Type/v1.0","attributes":[{"timestamp":{"type":"uint64"}},{"value":{"type":"float32"}}]}'
-            instance='{"timestamp":0,"value":0.1}'/>
-        <PVServerVariable name="pvxs-other"
-            channel="seq::test::another"
-            datatype='{"type":"seq::test::Type/v1.0","attributes":[{"timestamp":{"type":"uint64"}},{"value":{"type":"float32"}}]}'
-            instance='{"timestamp":0,"value":1.0}'/>
+            datatype='{"type":"seq::pva_write_test::Type/v1.0"}'/>
+        <Local name="old-pvxs-value"
+               type='{"type":"seq::pva_write_test::Type/v1.0"}'
+               value='{"value":1.0}'/>
+        <Local name="new-pvxs-value"
+               type='{"type":"seq::pva_write_test::Type/v1.0"}'
+               value='{"value":12.5}'/>
     </Workspace>
 </Procedure>)RAW";
 
-TEST(PVServerVariable, ProcedureVariable)
+static ccs::log::Func_t _log_handler = ccs::log::SetStdout();
+
+class PVAccessWriteInstructionTest : public ::testing::Test
 {
+protected:
+  PVAccessWriteInstructionTest();
+  virtual ~PVAccessWriteInstructionTest();
+
   sup::sequencer::gtest::NullUserInterface ui;
+  bool plugins_loaded;
+};
+
+// Tests
+
+TEST_F(PVAccessWriteInstructionTest, write_success)
+{
+  ASSERT_TRUE(plugins_loaded);
+
   auto proc = sup::sequencer::ParseProcedureString(PVACCESSWRITEPROCEDURE);
   ASSERT_TRUE(static_cast<bool>(proc));
   ASSERT_TRUE(proc->Setup());
@@ -91,5 +109,15 @@ TEST(PVServerVariable, ProcedureVariable)
 
   EXPECT_EQ(exec, sup::sequencer::ExecutionStatus::SUCCESS);
 }
+
+PVAccessWriteInstructionTest::PVAccessWriteInstructionTest()
+  : ui{}
+  , plugins_loaded{false}
+{
+  plugins_loaded = true; // sup::sequencer::LoadPlugin("../../../target/lib/libsequencer-pvxs.so")
+    // && sup::sequencer::LoadPlugin("../../../target/lib/libsequencer-misc.so");
+}
+
+PVAccessWriteInstructionTest::~PVAccessWriteInstructionTest() = default;
 
 #undef LOG_ALTERN_SRC
