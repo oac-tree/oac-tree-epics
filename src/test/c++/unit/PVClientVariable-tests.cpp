485b9c25d3d4ae36ab1e601797a6adb4dba64860
Gennady Pospelov <gennady.pospelov@gmail.com>
2021-10-28 17:14:21 +0200

    Implement unit test for PVClientVariable

--- src/test/c++/unit/PVClientVariable-tests.cpp
+++ src/test/c++/unit/PVClientVariable-tests.cpp
@@ -21,23 +21,25 @@
 
 #include "PVClientVariable.h"
 
+#include <SequenceParser.h>
+#include <UserInterface.h>
 #include <VariableRegistry.h>
-#include <common/PVAccessServer.h>
 #include <common/PVAccessClient.h>
+#include <common/PVAccessServer.h>
 #include <gtest/gtest.h>
 
 #include <algorithm>
 
-using sup::sequencer::PVClientVariable;
 using ccs::HelperTools::GetAttributeValue;
-using ccs::HelperTools::SetAttributeValue;
 using ccs::HelperTools::HasAttribute;
+using ccs::HelperTools::SetAttributeValue;
+using sup::sequencer::PVClientVariable;
 
 namespace
 {
 const size_t kSecond = 1e9;
 
-std::string CreateProcedureString(const std::string &body)
+std::string CreateProcedureString(const std::string& body)
 {
   static const std::string header{
       R"RAW(<?xml version="1.0" encoding="UTF-8"?>
@@ -51,10 +53,16 @@ std::string CreateProcedureString(const std::string &body)
   return header + body + footer;
 }
 
-}
+}  // namespace
 
 class PVClientVariableTest : public ::testing::Test
 {
+public:
+  class NullUserInterface : public sup::sequencer::UserInterface
+  {
+  public:
+    void UpdateInstructionStatusImpl(const sup::sequencer::Instruction* instruction) {}
+  };
 };
 
 TEST_F(PVClientVariableTest, VariableRegistration)
@@ -91,9 +99,13 @@ TEST_F(PVClientVariableTest, GetNonExistingValue)
   EXPECT_THROW(variable.GetValue(value), std::runtime_error);
 }
 
+//! Server creates a structure with the value.
+//! Check that we can get and set the value from PVClientVariable.
+
 TEST_F(PVClientVariableTest, PVAccessServerClientTest)
 {
-  const std::string kDataType=R"({"type":"testtype","attributes":[{"timestamp":{"type":"uint64"}},{"value":{"type":"float32"}}]})";
+  const std::string kDataType =
+      R"({"type":"testtype","attributes":[{"timestamp":{"type":"uint64"}},{"value":{"type":"float32"}}]})";
   const char* channel = "PVClientVariableTest:INTEGER";
 
   ::ccs::base::PVAccessServer server;
@@ -147,9 +159,65 @@ TEST_F(PVClientVariableTest, PVAccessServerClientTest)
   EXPECT_EQ(server_value, 142.0);
 }
 
-//! Structure with timestamp and value is created on server
+//! Structure with value is created on server.
+//! Procedure creates PVClientVariable and set new value to it.
+//! We expect that the value will be propagated to the server side.
 
 TEST_F(PVClientVariableTest, ExecuteProcedure)
 {
+  // preparing procedure
+  const std::string body{R"(
+    <Plugin>libsequencer-pvxs.so</Plugin>
+    <RegisterType jsontype='{"type":"seq::test::Type/v1.0","attributes":[{"value":{"type":"float32"}}]}'/>
+    <Sequence>
+        <Copy input="new-pvxs-value" output="pvxs-client-variable"/>
+        <Wait timeout="1.0"/>
+    </Sequence>
+    <Workspace>
+        <PVClientVariable name="pvxs-client-variable"
+            channel="PVClientVariableTest:INTEGER"
+            datatype='{"type":"seq::test::Type/v1.0"}'/>            
+        <Local name="new-pvxs-value"
+               type='{"type":"seq::test::Type/v1.0"}'
+               value='{"value":12.5}'/>
+    </Workspace>
+)"};
+  // preparing server
+  const std::string kDataType =
+      R"({"type":"seq::test::Type/v1.0","attributes":[{"value":{"type":"float32"}}]})";
+  const char* channel = "PVClientVariableTest:INTEGER";
+  ::ccs::base::PVAccessServer server;
 
+  // creating the variable
+  server.AddVariable(channel, ccs::types::AnyputVariable, kDataType.c_str());
+  server.Launch();
+  ccs::HelperTools::SleepFor(0.1 * kSecond);
+
+  // setting the value via PVAccessServer::GetVariable
+  ccs::types::float32 value = 42.1;
+  EXPECT_TRUE(SetAttributeValue(server.GetVariable(channel), "value", value));
+  EXPECT_TRUE(server.UpdateVariable(channel));  // without this call EXPECT below are failing
+  ccs::HelperTools::SleepFor(0.1 * kSecond);
+
+  // running the procedure
+  auto proc = sup::sequencer::ParseProcedureString(CreateProcedureString(body));
+  ASSERT_TRUE(proc.get() != nullptr);
+  ASSERT_TRUE(proc->Setup());
+
+  // executing procedure till success
+  NullUserInterface ui;
+  sup::sequencer::ExecutionStatus exec = sup::sequencer::ExecutionStatus::FAILURE;
+  do
+  {
+    (void)ccs::HelperTools::SleepFor(0.1 * kSecond);  // Let system breathe
+    proc->ExecuteSingle(&ui);
+    exec = proc->GetStatus();
+  } while ((sup::sequencer::ExecutionStatus::SUCCESS != exec)
+           && (sup::sequencer::ExecutionStatus::FAILURE != exec));
+  EXPECT_EQ(exec, sup::sequencer::ExecutionStatus::SUCCESS);
+
+  // reading from the server
+  ccs::types::float32 server_value = 0;
+  ASSERT_TRUE(GetAttributeValue(server.GetVariable(channel), "value", server_value));
+  EXPECT_EQ(server_value, 12.5);  // like in new-pvx-value LocalVariable
 }
