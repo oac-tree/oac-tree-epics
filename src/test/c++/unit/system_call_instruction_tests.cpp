6ee717a4a0257ac733e0e8d325849ce186e52481
Walter Van Herck <walter.vanherck@iter.org>
2022-10-12 12:38:03 +0000

    Add unit tests for SystemCall instruction

---
+++ src/test/c++/unit/system_call_instruction_tests.cpp
@@ -0,0 +1,66 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP Sequencer
+ *
+ * Description   : Unit test code
+ *
+ * Author        : B.Bauvir (IO)
+ *
+ * Copyright (c) : 2010-2020 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include "null_user_interface.h"
+
+#include <sup/sequencer/instruction.h>
+#include <sup/sequencer/instruction_registry.h>
+#include <sup/sequencer/procedure.h>
+
+#include <gtest/gtest.h>
+
+TEST(SystemCallInstruction, Execute_missing)
+{
+  auto instruction = sup::sequencer::GlobalInstructionRegistry().Create("SystemCall");
+  ASSERT_TRUE(static_cast<bool>(instruction));
+
+  sup::sequencer::unit_test_helper::NullUserInterface ui;
+  sup::sequencer::Procedure proc;
+  EXPECT_FALSE(instruction->Setup(proc));
+}
+
+TEST(SystemCallInstruction, Execute_success)
+{
+  auto instruction = sup::sequencer::GlobalInstructionRegistry().Create("SystemCall");
+  ASSERT_TRUE(static_cast<bool>(instruction));
+
+  EXPECT_TRUE(instruction->AddAttribute("command", "/usr/bin/ls /tmp &> /dev/null"));
+
+  sup::sequencer::unit_test_helper::NullUserInterface ui;
+  sup::sequencer::Procedure proc;
+  EXPECT_TRUE(instruction->Setup(proc));
+  instruction->ExecuteSingle(&ui, nullptr);
+  EXPECT_EQ(instruction->GetStatus(), sup::sequencer::ExecutionStatus::SUCCESS);
+}
+
+TEST(SystemCallInstruction, Execute_error)
+{
+  auto instruction = sup::sequencer::GlobalInstructionRegistry().Create("SystemCall");
+  ASSERT_TRUE(static_cast<bool>(instruction));
+
+  EXPECT_TRUE(instruction->AddAttribute("command", "/usr/bin/undefined &> /dev/null"));
+
+  sup::sequencer::unit_test_helper::NullUserInterface ui;
+  sup::sequencer::Procedure proc;
+  EXPECT_TRUE(instruction->Setup(proc));
+  instruction->ExecuteSingle(&ui, nullptr);
+  EXPECT_EQ(instruction->GetStatus(), sup::sequencer::ExecutionStatus::FAILURE);
+}
