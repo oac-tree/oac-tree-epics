3ca535617fd58cbe5f6d65e1c9cb0ce73ba08a2e
Walter Van Herck <walter.vanherck@iter.org>
2022-11-24 14:50:16 +0000

    Add PvAccessServer variable

---
+++ src/main/c++/pvxs/pv_access_server_variable.cpp
@@ -0,0 +1,135 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : CODAC Supervision and Automation (SUP) Sequencer component
+ *
+ * Description   : Variable plugin implementation
+ *
+ * Author        : Gennady Pospelov
+ *
+ * Copyright (c) : 2010-2020 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include "pv_access_server_variable.h"
+
+#include "pv_access_helper.h"
+
+#include <sup/sequencer/variable_registry.h>
+
+#include <sup/dto/anyvalue_helper.h>
+#include <sup/dto/json_type_parser.h>
+#include <sup/dto/json_value_parser.h>
+#include <sup/epics/pv_access_server.h>
+
+namespace sup
+{
+namespace sequencer
+{
+const std::string PvAccessServerVariable::Type = "PvAccessServer";
+static bool PvAccessServerVariable_initialised_flag =
+  RegisterGlobalVariable<PvAccessServerVariable>();
+
+const std::string CHANNEL_ATTRIBUTE_NAME = "channel";
+const std::string TYPE_ATTRIBUTE_NAME = "type";
+const std::string VALUE_ATTRIBUTE_NAME = "value";
+
+PvAccessServerVariable::PvAccessServerVariable()
+  : Variable(PvAccessServerVariable::Type)
+  , m_type{}
+  , m_server{}
+{}
+
+PvAccessServerVariable::~PvAccessServerVariable() = default;
+
+bool PvAccessServerVariable::GetValueImpl(sup::dto::AnyValue& value) const
+{
+  if (!m_server || !m_type || !HasAttribute(CHANNEL_ATTRIBUTE_NAME))
+  {
+    return false;
+  }
+  auto converted_val = pv_access_helper::ConvertToTypedAnyValue(
+    m_server->GetValue(GetAttribute(CHANNEL_ATTRIBUTE_NAME)), *m_type);
+  return !sup::dto::IsEmptyValue(converted_val) && sup::dto::TryConvert(value, converted_val);
+}
+
+bool PvAccessServerVariable::SetValueImpl(const sup::dto::AnyValue& value)
+{
+  if (!m_server || !m_type || !HasAttribute(CHANNEL_ATTRIBUTE_NAME))
+  {
+    return false;
+  }
+  sup::dto::AnyValue copy(*m_type);
+  if (!sup::dto::TryConvert(copy, value))
+  {
+    return false;
+  }
+  return m_server->SetValue(GetAttribute(CHANNEL_ATTRIBUTE_NAME),
+                            pv_access_helper::PackIntoStructIfScalar(copy));
+}
+
+bool PvAccessServerVariable::IsAvailableImpl() const
+{
+  if (!m_server || !m_type || !HasAttribute(CHANNEL_ATTRIBUTE_NAME))
+  {
+    return false;
+  }
+  auto value = pv_access_helper::ConvertToTypedAnyValue(
+    m_server->GetValue(GetAttribute(CHANNEL_ATTRIBUTE_NAME)), *m_type);
+  return !sup::dto::IsEmptyValue(value);
+}
+
+bool PvAccessServerVariable::SetupImpl(const sup::dto::AnyTypeRegistry& registry)
+{
+  if (!HasAttribute(CHANNEL_ATTRIBUTE_NAME) || !HasAttribute(TYPE_ATTRIBUTE_NAME))
+  {
+    return false;
+  }
+  sup::dto::JSONAnyTypeParser parser;
+  if (!parser.ParseString(GetAttribute(TYPE_ATTRIBUTE_NAME), &registry))
+  {
+    return false;
+  }
+  m_type.reset(new sup::dto::AnyType(parser.MoveAnyType()));
+  sup::dto::AnyValue val(*m_type);
+  if (HasAttribute(VALUE_ATTRIBUTE_NAME))
+  {
+    sup::dto::JSONAnyValueParser value_parser;
+    if (!value_parser.TypedParseString(*m_type, GetAttribute(VALUE_ATTRIBUTE_NAME)))
+    {
+      return false;
+    }
+    val = value_parser.MoveAnyValue();
+  }
+  // Avoid dependence on destruction order of m_server and m_type.
+  sup::dto::AnyType type_copy{*m_type};
+  auto callback = [this, type_copy](const std::string&, const sup::dto::AnyValue& value)
+  {
+    auto typed_value = pv_access_helper::ConvertToTypedAnyValue(value, type_copy);
+    Notify(typed_value);
+    return;
+  };
+  m_server.reset(new epics::PvAccessServer(callback));
+  auto start_value = pv_access_helper::PackIntoStructIfScalar(val);
+  m_server->AddVariable(GetAttribute(CHANNEL_ATTRIBUTE_NAME), start_value);
+  m_server->Start();
+  return true;
+}
+
+void PvAccessServerVariable::ResetImpl()
+{
+  m_server.reset();
+  m_type.reset();
+}
+
+}  // namespace sequencer
+
+}  // namespace sup
---
+++ src/main/c++/pvxs/pv_access_server_variable.h
@@ -0,0 +1,73 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : CODAC Supervision and Automation (SUP) Sequencer component
+ *
+ * Description   : Variable plugin implementation
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#ifndef SUP_SEQUENCER_PLUGIN_EPICS_PV_ACCESS_SERVER_VARIABLE_H_
+#define SUP_SEQUENCER_PLUGIN_EPICS_PV_ACCESS_SERVER_VARIABLE_H_
+
+#include <sup/sequencer/variable.h>
+
+#include <memory>
+
+namespace sup
+{
+namespace epics
+{
+class PvAccessServer;
+}  // namespace epics
+
+namespace sequencer
+{
+/**
+ * @brief Workspace variable associated with a locally hosted pvAccess server.
+ * The variable is configured with mandatory 'channel' and 'type' attributes. An initial value can
+ * be provided with the optional 'value' attribute.
+ * @code
+     <Workspace>
+       <PvAccessServer name="pvxs-variable"
+         channel="seq::pvxs::servervariable"
+         type='{"type":"seq::pvxs::Type/v1.0","attributes":[{"timestamp":{"type":"uint64"}},{"value":{"type":"float32"}}]}'/>
+     </Workspace>
+   @endcode
+ */
+class PvAccessServerVariable : public Variable
+{
+public:
+  PvAccessServerVariable();
+  ~PvAccessServerVariable();
+
+  static const std::string Type;
+
+private:
+  bool GetValueImpl(sup::dto::AnyValue &value) const override;
+  bool SetValueImpl(const sup::dto::AnyValue &value) override;
+  bool IsAvailableImpl() const override;
+  bool SetupImpl(const sup::dto::AnyTypeRegistry& registry) override;
+  void ResetImpl() override;
+
+  std::unique_ptr<sup::dto::AnyType> m_type;
+  std::unique_ptr<epics::PvAccessServer> m_server;
+};
+
+}  // namespace sequencer
+
+}  // namespace sup
+
+#endif  // SUP_SEQUENCER_PLUGIN_EPICS_PV_ACCESS_SERVER_VARIABLE_H_
