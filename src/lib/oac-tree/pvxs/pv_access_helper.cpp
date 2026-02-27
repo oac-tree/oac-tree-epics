b77355f419c25d223627d4b9c3eeeb7f2ec85b85
Walter Van Herck <walter.vanherck@iter.org>
2026-02-27 15:53:55 +0000

    Add helper function to do partial conversion of AnyValues

--- src/lib/oac-tree/pvxs/pv_access_helper.cpp
+++ src/lib/oac-tree/pvxs/pv_access_helper.cpp
@@ -24,12 +24,20 @@
 
 #include <sup/dto/anyvalue_helper.h>
 
+#include <deque>
+
 namespace sup
 {
 namespace oac_tree
 {
 namespace pv_access_helper
 {
+namespace
+{
+// Try to convert the src value to dest, allowing for dest to only contain a subset of fields of
+// src. dest is assumed to be non-empty and non-scalar.
+bool TryPartialConvert(sup::dto::AnyValue& dest, const sup::dto::AnyValue& src);
+}  // unnamed namespace
 
 sup::dto::AnyValue ConvertToTypedAnyValue(const sup::dto::AnyValue& value,
                                           const sup::dto::AnyType& anytype)
@@ -73,6 +81,55 @@ PvAccessSharedServerRegistry& GetSharedPvAccessServerRegistry()
   static PvAccessSharedServerRegistry shared_registry{};
   return shared_registry;
 }
+
+namespace
+{
+
+struct PartialConvertNode
+{
+  sup::dto::AnyValue* m_dest;
+  const sup::dto::AnyValue* m_src;
+};
+
+bool TryPartialConvert(sup::dto::AnyValue& dest, const sup::dto::AnyValue& src)
+{
+  // Prevent failure after partial updates:
+  auto copy = dest;
+  std::deque<PartialConvertNode> stack{};
+  stack.push_back(PartialConvertNode{std::addressof(copy), std::addressof(src)});
+  while (!stack.empty())
+  {
+    auto& front = stack.front();
+    auto* current_dest = front.m_dest;
+    auto* current_src = front.m_src;
+    if (sup::dto::IsStructValue(*current_dest))
+    {
+      auto mem_names = current_dest->MemberNames();
+      for (const auto& mem_name : mem_names)
+      {
+        if (!current_src->HasField(mem_name))
+        {
+          return false;
+        }
+        auto* dest_mem = std::addressof((*current_dest)[mem_name]);
+        auto* src_mem = std::addressof((*current_src)[mem_name]);
+        stack.push_back(PartialConvertNode{dest_mem, src_mem});
+      }
+    }
+    else
+    {
+      if (!sup::dto::TryConvert(*current_dest, *current_src))
+      {
+        return false;
+      }
+    }
+    stack.pop_front();
+  }
+  dest = copy;
+  return true;
+}
+
+}  // unnamed namespace
 
 }  // namespace pv_access_helper
 
